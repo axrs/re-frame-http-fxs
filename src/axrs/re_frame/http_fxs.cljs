9584cec5b5c1aeacd9befc35ea60e9974d92cb1b
Alexander Scott <xander@cadcoder.com>
2017-04-25 14:42:09 +1000

    Test conjining results

--- src/axrs/re_frame/http_fxs.cljs
+++ src/axrs/re_frame/http_fxs.cljs
@@ -1,1 +1,63 @@
-(ns axrs.re-frame.http-fxs)
+(ns axrs.re-frame.http-fxs
+  (:require
+    [re-frame.core :refer [reg-event-fx debug subscribe dispatch]]
+    [clojure.string :as string]))
+
+(declare clj->jskw)
+
+(defn- key->jskw [k]
+  (if (satisfies? IEncodeJS k)
+    (-clj->js k)
+    (if (or (string? k)
+            (number? k)
+            (keyword? k)
+            (symbol? k))
+      (clj->jskw k)
+      (pr-str k))))
+
+(defn- clj->jskw
+  "Note: Altered from cljs.core to encode keywords
+  Recursively transforms ClojureScript values to JavaScript.
+  sets/vectors/lists become Arrays, Keywords and Symbol become Strings,
+  Maps become Objects. Arbitrary keys are encoded to by key->js."
+  [x]
+  (when-not (nil? x)
+    (if (satisfies? IEncodeJS x)
+      (-clj->js x)
+      (cond
+        (keyword? x) (string/replace (str (keyword x)) #"^:" "")
+        (symbol? x) (str x)
+        (map? x) (let [m (js-obj)]
+                   (doseq [[k v] x]
+                     (aset m (key->jskw k) (clj->jskw v)))
+                   m)
+        (coll? x) (let [arr (array)]
+                    (doseq [x (map clj->jskw x)]
+                      (.push arr x))
+                    arr)
+        :else x))))
+
+(defn- append-result
+  "Appends the HTTP result object to the end of all after requests"
+  [result afters]
+  (mapv #(conj % result) afters))
+
+(reg-event-fx ::http-result
+              (fn [db [_ afters result]]
+                {:dispatch-n (append-result result afters)}))
+
+(defn json-request
+  "Issues a JSON request to a specified URL, sending a provided body and dispatching success or
+  error handlers respectively"
+  [_ [_ {:keys [type url body after-success after-errors]
+         :or   {type :post body nil after-success [] after-errors [[:http-error]]}
+         :as   request}]]
+  {:http-xhrio {:method          (keyword type)
+                :uri             url
+                :format          (ajax/json-request-format)
+                :response-format (ajax/json-response-format {:keywords? true})
+                :params          (clj->jskw body)
+                :on-success      [::http-result after-success]
+                :on-failure      [::http-result after-errors]}})
+
+(reg-event-fx :json-request json-request)
--- test/axrs/re_frame/http_fxs_test.cljs
+++ test/axrs/re_frame/http_fxs_test.cljs
@@ -1,8 +1,35 @@
 (ns axrs.re-frame.http-fxs-test
   (:require
     [cljs.test :refer-macros [is are deftest testing use-fixtures]]
-    [axrs.re-frame.http-fxs]))
+    [axrs.re-frame.http-fxs :refer [clj->jskw]]))
 
-(deftest testing-1-1
-  (testing "Testing that 1+1 = 2"
-    (is (= 1 2))))
+(defn- js->str [s] (.stringify js/JSON s))
+
+(deftest test-clj->jskw
+  (testing "Replacement clj->js method to include namespaced keywords"
+    (let [m {:black/knight "None shall pass."
+             :arthur       "What?"
+             :knight       "None shall pass."
+             :king/arthur  ["I have no quarrel with you," "good Sir Knight," "but I must cross this bridge."]
+             :pause        5
+             :story        "Arthur cuts off the Black Knight's left arm."
+             :scene        nil}]
+      (is (= "{\"knight\":\"None shall pass.\",\"arthur\":[\"I have no quarrel with you,\",\"good Sir Knight,\",\"but I must cross this bridge.\"],\"pause\":5,\"story\":\"Arthur cuts off the Black Knight's left arm.\",\"scene\":null}"
+             (js->str (clj->js m))))
+      (is (= "{\"black/knight\":\"None shall pass.\",\"arthur\":\"What?\",\"knight\":\"None shall pass.\",\"king/arthur\":[\"I have no quarrel with you,\",\"good Sir Knight,\",\"but I must cross this bridge.\"],\"pause\":5,\"story\":\"Arthur cuts off the Black Knight's left arm.\",\"scene\":null}"
+             (js->str (clj->jskw m))))))
+
+  (testing "It should convert nested values aswell"
+    (let [m {:black/knight {:says "None shall pass."}
+             :king/arthur  {:questioning/reply "What?"}}]
+      (is (= "{\"black/knight\":{\"says\":\"None shall pass.\"},\"king/arthur\":{\"questioning/reply\":\"What?\"}}"
+             (js->str (clj->jskw m)))))))
+
+(deftest appending-http-result
+  (testing "It should append the http results to to end of each provided vector"
+    (let [d [[:black-knight {:says "None shall pass."}]
+             [:king/arthur {:reply "What?"}]]
+          expected {:king/arthur "I have no quarrel with you, good Sir Knight, but I must cross this bridge."}
+          result (axrs.re-frame.http-fxs/append-result expected d)]
+      (is (= (conj (first d) expected)) (first result))
+      (is (= (conj (second d) expected)) (second result)))))
